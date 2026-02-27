5eca288ac4d49c0c7415856cb58365e1b63d7719
NgocHan-Tran <hann030904@gmail.com>
2026-02-28 00:02:55 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -4,11 +4,25 @@ const cors = require("cors");
 const app = express();
 
 const contactRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 
 app.use(cors());
 app.use(express.json());
 app.use("/api/contacts", contactRouter);
-
+// handle 404 response
+app.use((req, res, next) => {
+    // Code ở đây sẽ chạy khi không có route được định nghĩa nào
+    // khớp với yêu cầu. Gọi next() đề chuyển sang middleware xử lý lỗi
+    return next(new ApiError(404, "Resource not found"));
+});
+// define error-handling middleware last, after other app.use() and routes calls
+app.use((err, req, res, next) => {
+    // Middleware xử lý lỗi tập trung.
+    // Trong các đoạn code xử lý ở các route, gọi next(error) se chuyển về middleware xử lý lỗi này
+    return res.status(error.statusCode || 500) .json({
+    message: error.message || "Internal Server Error",
+});
+});
 app.get("/", (req,res) => {
     res.json({message: "Welcome to contact book application."});
 });
